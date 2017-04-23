9b1d0d1b5000772e70016ceea410e22ccd46a6c9
devis.rossini <devis.rossini@gmail.com>
2017-04-23 21:28:08 +0200

    ComponentController updated.

--- Source/ShootMeAround/ComponentController.cpp
+++ Source/ShootMeAround/ComponentController.cpp
@@ -1,40 +1,28 @@
 #include "ShootMeAround.h"
-#include "Array.h"
-#include "Vector.h"
-#include "GameFramework/Actor.h"
-#include "Containers/ContainersFwd.h"
-#include "GameFramework/PlayerController.h"
-#include "Components/BillboardComponent.h"
 #include "ShootMeAroundCharacter.h"
+
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+
+#include "Components/BillboardComponent.h"
+
 #include "Engine/StaticMeshActor.h"
+
 #include "ComponentController.h"
-
-#pragma optimize ("",off)
-
-const float AComponentController::MAX_TIMER_UPDATE_COPY = 0.0001f;
-
-void AComponentController::AddBullet(AActor * NewBullet)
-{
-	OriginalBulletInstances.Add(NewBullet);
-
-	if (ActivateCopy)
-	{
-		for (ArrayOfTranslatedObject &ToAddBullet : CopiedBulletInstances)
-		{
-			AActor* CopiedBullet = SpawnCopy(NewBullet, ToAddBullet.Value);
-			ToAddBullet.Key.Add(CopiedBullet);
-		}
-	}
-
-}
 
 AComponentController::AComponentController()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
 	Billboard = CreateDefaultSubobject<UBillboardComponent>(TEXT("Billboard"));
 
-	this->SetActorTickInterval(MAX_TIMER_UPDATE_COPY);
+	RightTranslation = 0.f;
+	FrontTranslation = 0.f;
+}
 
+void AComponentController::AddBullet(AActor* NewBullet)
+{
+	OriginalBulletInstances.Add(NewBullet);
 }
 
 void AComponentController::BeginPlay()
@@ -42,91 +30,26 @@ void AComponentController::BeginPlay()
 	Super::BeginPlay();
 
 	FillOriginalObjectsArray();
-
-	if (ActivateCopy)
-	{
-		CreateCopiedInstances();
-	}
-
 }
 
 void AComponentController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	RightTranslation = World->GetComponentsBoundingBox().GetSize().Y;
-	FrontTranslation = World->GetComponentsBoundingBox().GetSize().X;
-
 	TeleportObjectsIfEscaping();
 
 	UpdateTriggerLimit();
-
-	if (ActivateCopy)
-	{
-		UpdateCopyTransform();
-	}
-
-}
-
-void AComponentController::UpdateCopyTransform()
-{
-	int OriginalObjectCounter = 0;
-
-	for (ArrayOfTranslatedObject ObjectsToTranslate : CopiedObjectInstances)
-	{
-
-		OriginalObjectCounter = 0;
-
-		for (AActor* ToTranslate : ObjectsToTranslate.Key)
-		{
-			TranslateObject(ToTranslate, OriginalObjectInstances[OriginalObjectCounter], ObjectsToTranslate.Value);
-
-			++OriginalObjectCounter;
-		}
-
-	}
-
-	for (ArrayOfTranslatedCharacter CharacterToTranslate : CopiedCharacterInstances)
-	{
-		OriginalObjectCounter = 0;
-
-		for (ACharacter* ToTranslate : CharacterToTranslate.Key)
-		{
-			TranslateObject(ToTranslate, OriginalCharacterInstances[OriginalObjectCounter], CharacterToTranslate.Value);
-
-			ToTranslate->GetMovementComponent()->Velocity = OriginalCharacterInstances[OriginalObjectCounter]->GetMovementComponent()->Velocity;
-
-			++OriginalObjectCounter;
-		}
-
-	}
-
-	for (ArrayOfTranslatedObject BulletToTranslate : CopiedBulletInstances)
-	{
-
-		OriginalObjectCounter = 0;
-
-		for (AActor* ToTranslate : BulletToTranslate.Key)
-		{
-			TranslateObject(ToTranslate, OriginalBulletInstances[OriginalObjectCounter], BulletToTranslate.Value);
-
-			++OriginalObjectCounter;
-		}
-
-	}
 }
 
 void AComponentController::FillOriginalObjectsArray()
 {
-
 	FConstPlayerControllerIterator PlayerControllers = GetWorld()->GetPlayerControllerIterator();
-	for (int PlayerControllerIndex = 0; PlayerControllerIndex < NumberOfPlayers; ++PlayerControllerIndex)
+	
+	for (int PlayerControllerIndex = 0; PlayerControllerIndex < GetWorld()->PlayerNum; ++PlayerControllerIndex, ++PlayerControllers)
 	{
 		TAutoWeakObjectPtr<APlayerController> ActualPlayer = *PlayerControllers;
 		ACharacter* PlayerCharacter = ActualPlayer->GetCharacter();
-		PlayerCharacter->GetMesh()->MasterPoseComponent;
 		OriginalCharacterInstances.Add(PlayerCharacter);
-		PlayerControllers++;
 	}
 
 	for (AActor *Object : Objects)
@@ -135,116 +58,18 @@ void AComponentController::FillOriginalObjectsArray()
 	}
 }
 
-void AComponentController::CreateCopiedInstances()
-{
-	RightTranslation = World->GetComponentsBoundingBox().GetSize().Y;
-	FrontTranslation = World->GetComponentsBoundingBox().GetSize().X;
-
-	//Right  
-
-	CreateCopiedInstancesWithTranslation(0, RightTranslation);
-
-	//Left  
-
-	CreateCopiedInstancesWithTranslation(0, -RightTranslation);
-
-	//Front  
-
-	CreateCopiedInstancesWithTranslation(FrontTranslation, 0);
-
-	//Behind  
-
-	CreateCopiedInstancesWithTranslation(-FrontTranslation, 0);
-
-	//Front-Right  
-
-	CreateCopiedInstancesWithTranslation(FrontTranslation, RightTranslation);
-
-	//Front-Left  
-
-	CreateCopiedInstancesWithTranslation(FrontTranslation, -RightTranslation);
-
-	//Behind-Right  
-
-	CreateCopiedInstancesWithTranslation(-FrontTranslation, RightTranslation);
-
-	//Behind-Left  
-
-	CreateCopiedInstancesWithTranslation(-FrontTranslation, -RightTranslation);
-
-
-}
-
-void AComponentController::CreateCopiedInstancesWithTranslation(float XTranslation, float YTranslation)
-{
-
-	FActorSpawnParameters WorldParameters;
-	WorldParameters.Template = World;
-	AStaticMeshActor * TranslatedWorld = GetWorld()->SpawnActor<class AStaticMeshActor>(World->GetClass(), WorldParameters);
-	TranslatedWorld->SetMobility(EComponentMobility::Movable);
-
-	FVector WorldLocationToTranslate = TranslatedWorld->GetActorLocation();
-	WorldLocationToTranslate.X += XTranslation;
-	WorldLocationToTranslate.Y += YTranslation;
-	TranslatedWorld->SetActorLocation(WorldLocationToTranslate);
-
-	ArrayOfTranslatedObject TranslatedCopiedObjectInstances;
-	ArrayOfTranslatedCharacter TranslatedCopiedCharacterInstances;
-	ArrayOfTranslatedObject TranslatedCopiedBulletInstances;
-
-	TranslatedCopiedObjectInstances.Value = TranslatedWorld->GetActorLocation() - World->GetActorLocation();
-	TranslatedCopiedCharacterInstances.Value = TranslatedWorld->GetActorLocation() - World->GetActorLocation();
-	TranslatedCopiedBulletInstances.Value = TranslatedWorld->GetActorLocation() - World->GetActorLocation();
-
-	for (AActor* Object : OriginalObjectInstances)
-	{
-		TranslatedCopiedObjectInstances.Key.Add(SpawnCopy(Object, TranslatedCopiedObjectInstances.Value, true));
-	}
-
-	for (AActor* Character : OriginalCharacterInstances)
-	{
-		AActor* ToAdd = Cast<AActor>(SpawnCopy(Character, TranslatedCopiedCharacterInstances.Value));
-		TranslatedCopiedCharacterInstances.Key.Add(Cast<ACharacter>(ToAdd));
-	}
-
-	CopiedObjectInstances.Add(TranslatedCopiedObjectInstances);
-	CopiedCharacterInstances.Add(TranslatedCopiedCharacterInstances);
-	CopiedBulletInstances.Add(TranslatedCopiedBulletInstances);
-}
-
-AActor* AComponentController::SpawnCopy(AActor* OriginalObject, FVector Translation, bool SetTemplate)
-{
-	if (OriginalObject == nullptr) return nullptr;
-
-	FActorSpawnParameters ObjectParameters;
-	AActor* TranslatedObject;
-	if (SetTemplate)
-	{
-		ObjectParameters.Template = OriginalObject;
-		TranslatedObject = GetWorld()->SpawnActor<class AActor>(OriginalObject->GetClass(), ObjectParameters);
-	}
-	else
-	{
-		TranslatedObject = GetWorld()->SpawnActor<class AActor>(OriginalObject->GetClass(), OriginalObject->GetActorTransform(), ObjectParameters);
-	}
-
-	FVector ObjectLocationToTranslate = TranslatedObject->GetActorLocation();
-	ObjectLocationToTranslate += Translation;
-	TranslatedObject->SetActorLocation(ObjectLocationToTranslate);
-
-	return TranslatedObject;
-}
-
 void AComponentController::TeleportObjectsIfEscaping()
 {
 	for (AActor* Object : OriginalObjectInstances)
 	{
 		TeleportIfEscaping(Object);
 	}
+
 	for (AActor* Object : OriginalCharacterInstances)
 	{
 		TeleportIfEscaping(Object);
 	}
+
 	for (AActor* Object : OriginalBulletInstances)
 	{
 		TeleportIfEscaping(Object);
@@ -253,128 +78,63 @@ void AComponentController::TeleportObjectsIfEscaping()
 
 void AComponentController::TeleportIfEscaping(AActor* Object)
 {
-
-	if (Object == nullptr) return;
-
-	if (Object->GetActorLocation().X > FrontTranslation/2.0f)
+	if (Object == nullptr || !Object->IsValidLowLevel())
 	{
-   		Object->SetActorLocation(Object->GetActorLocation() + FVector(-FrontTranslation, 0, 0));
-	}
-	else if (Object->GetActorLocation().X < -FrontTranslation / 2.0f)
-	{
-		Object->SetActorLocation(Object->GetActorLocation() + FVector(FrontTranslation, 0, 0));
+		return;
 	}
 
-	if (Object->GetActorLocation().Y > RightTranslation / 2.0f)
+	FVector CurrLocation = Object->GetActorLocation();
+
+	// Check top.
+
+	if (CurrLocation.X > FrontTranslation / 2.0f)
 	{
-		Object->SetActorLocation(Object->GetActorLocation() + FVector(0, -RightTranslation, 0));
+		FVector NewLocation = CurrLocation;
+		NewLocation.X -= FrontTranslation;
+
+   		Object->SetActorLocation(NewLocation);
 	}
-	else if (Object->GetActorLocation().Y < -RightTranslation / 2.0f)
+	else if (CurrLocation.X < -FrontTranslation / 2.0f) // Check bottom.
 	{
-		Object->SetActorLocation(Object->GetActorLocation() + FVector(0, RightTranslation, 0));
-	}
-}
+		FVector NewLocation = CurrLocation;
+		NewLocation.X += FrontTranslation;
 
-void AComponentController::TranslateObject(AActor* ToTranslate, AActor* OriginalObject, const FVector &TranslateValue)
-{
-	if (ToTranslate != nullptr && ToTranslate->IsValidLowLevel())
-	{
-		if (OriginalObject->IsValidLowLevel())
-		{
-			FTransform TransformToTranslate = OriginalObject->GetActorTransform();
-			TransformToTranslate.SetLocation(TransformToTranslate.GetLocation() - TranslateValue);
-			ToTranslate->SetActorTransform(TransformToTranslate);
-		}
-	}
-}
-
-void AComponentController::SimulateEventOnCopy(ACharacter* Original, EventType Event)
-{
-
-	int OriginalObjectCounter = 0;
-
-	for (ACharacter* Searched : OriginalCharacterInstances)
-	{
-		if (Original == Searched)
-		{
-			for (ACharacter* ToEvent : CopiedCharacterInstances[OriginalObjectCounter].Key)
-			{
-				switch (Event)
-				{
-				case EventType::START_FIRE:
-						break;
-				case EventType::STOP_FIRE:
-						break;
-				case EventType::CROUCH:
-						break;
-				case EventType::UNCROUCH:
-						break;
-				}
-			}
-			break;
-		}
-
-		++OriginalObjectCounter;
-	}
-}
-
-void AComponentController::StartFire(ACharacter* Which)
-{
-
-	if (ActivateCopy)
-	{
-		SimulateEventOnCopy(Which, EventType::START_FIRE);
+		Object->SetActorLocation(NewLocation);
 	}
 
-}
+	// Check right.
 
-void AComponentController::StopFire(ACharacter* Which)
-{
-
-	if (ActivateCopy)
+	if (CurrLocation.Y > RightTranslation / 2.0f)
 	{
-		SimulateEventOnCopy(Which, EventType::STOP_FIRE);
+		FVector NewLocation = CurrLocation;
+		NewLocation.Y -= RightTranslation;
+
+		Object->SetActorLocation(NewLocation);
 	}
-
-}
-
-void AComponentController::Crouch(ACharacter* Which)
-{
-
-	if (ActivateCopy)
+	else if (CurrLocation.Y < -RightTranslation / 2.0f) // Check left.
 	{
-		SimulateEventOnCopy(Which, EventType::CROUCH);
+		FVector NewLocation = CurrLocation;
+		NewLocation.Y += RightTranslation;
+
+		Object->SetActorLocation(NewLocation);
 	}
-
-}
-
-void AComponentController::UnCrouch(ACharacter* Which)
-{
-
-	if (ActivateCopy)
-	{
-		SimulateEventOnCopy(Which, EventType::UNCROUCH);
-	}
-
 }
 
 void AComponentController::UpdateTriggerLimit()
 {
+	RightTranslation = World->GetComponentsBoundingBox().GetSize().Y;
+	FrontTranslation = World->GetComponentsBoundingBox().GetSize().X;
 
 	float WidthRadius = FrontTranslation /2.0f;
-	float HeightRadius = RightTranslation/2.0f;
+	float HeightRadius = RightTranslation / 2.0f;
+
 	FVector WorldLocation = World->GetActorLocation();
 
-	float TriggerLimitZ= PositiveX->GetActorLocation().Z;
+	float TriggerLimitZ = PositiveX->GetActorLocation().Z;
 
-	PositiveX->SetActorLocation({ WorldLocation.X + (WidthRadius + DistanceFromBorder),WorldLocation.Y, TriggerLimitZ });
+	PositiveX->SetActorLocation({ WorldLocation.X + WidthRadius, WorldLocation.Y, TriggerLimitZ });
+	PositiveY->SetActorLocation({ WorldLocation.X, WorldLocation.Y + HeightRadius, TriggerLimitZ });
 
-	NegativeX->SetActorLocation({ WorldLocation.X - (WidthRadius + DistanceFromBorder),WorldLocation.Y, TriggerLimitZ });
-
-	PositiveY->SetActorLocation({ WorldLocation.X, WorldLocation.Y + (HeightRadius + DistanceFromBorder), TriggerLimitZ });
-
-	NegativeY->SetActorLocation({ WorldLocation.X, WorldLocation.Y - (HeightRadius + DistanceFromBorder), TriggerLimitZ });
-
+	NegativeX->SetActorLocation({ WorldLocation.X - WidthRadius, WorldLocation.Y, TriggerLimitZ });
+	NegativeY->SetActorLocation({ WorldLocation.X, WorldLocation.Y - HeightRadius, TriggerLimitZ });
 }
-
-
