6cb6179d0f2564effd554e7f042f19ea3b69a979
ELIA DUCCESCHI <id249ckv@ateneo.univr.it>
2017-04-23 04:38:24 +0200

    - Aggiunto ComponentController

---
+++ Source/ShootMeAround/ComponentController.cpp
@@ -0,0 +1,272 @@
+#include "ShootMeAround.h"
+#include "Array.h"
+#include "Vector.h"
+#include "GameFramework/Actor.h"
+#include "Containers/ContainersFwd.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/BillboardComponent.h"
+#include "ShootMeAroundCharacter.h"
+#include "Engine/StaticMeshActor.h"
+#include "ComponentController.h"
+
+const float AComponentController::MAX_TIMER_UPDATE_COPY = 0.001f;
+
+void AComponentController::AddBullet(AActor * NewBullet)
+{
+	OriginalBulletInstances.Add(NewBullet);
+
+	for (ArrayOfTranslatedObject &ToAddBullet : CopiedBulletInstances)
+	{
+		AActor* CopiedBullet = SpawnCopy(NewBullet, ToAddBullet.Value);
+		ToAddBullet.Key.Add(CopiedBullet);
+	}
+
+}
+
+AComponentController::AComponentController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	Billboard = CreateDefaultSubobject<UBillboardComponent>(TEXT("Billboard"));
+
+	this->SetActorTickInterval(MAX_TIMER_UPDATE_COPY);
+
+}
+
+void AComponentController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FillOriginalObjectsArray();
+	CreateCopiedInstances();
+
+}
+
+void AComponentController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TeleportObjectsIfEscaping();
+
+	UpdateCopyTransform();
+
+}
+
+void AComponentController::UpdateCopyTransform()
+{
+	int OriginalObjectCounter = 0;
+
+	for (ArrayOfTranslatedObject ObjectsToTranslate : CopiedObjectInstances)
+	{
+
+		OriginalObjectCounter = 0;
+
+		for (AActor* ToTranslate : ObjectsToTranslate.Key)
+		{
+			TranslateObject(ToTranslate, OriginalObjectInstances[OriginalObjectCounter], ObjectsToTranslate.Value);
+
+			++OriginalObjectCounter;
+		}
+
+	}
+
+	for (ArrayOfTranslatedCharacter CharacterToTranslate : CopiedCharacterInstances)
+	{
+		OriginalObjectCounter = 0;
+
+		for (ACharacter* ToTranslate : CharacterToTranslate.Key)
+		{
+			TranslateObject(ToTranslate, OriginalCharacterInstances[OriginalObjectCounter], CharacterToTranslate.Value);
+
+			++OriginalObjectCounter;
+		}
+
+	}
+
+	for (ArrayOfTranslatedObject BulletToTranslate : CopiedBulletInstances)
+	{
+
+		OriginalObjectCounter = 0;
+
+		for (AActor* ToTranslate : BulletToTranslate.Key)
+		{
+			TranslateObject(ToTranslate, OriginalBulletInstances[OriginalObjectCounter], BulletToTranslate.Value);
+
+			++OriginalObjectCounter;
+		}
+
+	}
+}
+
+void AComponentController::FillOriginalObjectsArray()
+{
+
+	FConstPlayerControllerIterator PlayerControllers = GetWorld()->GetPlayerControllerIterator();
+	for (int PlayerControllerIndex = 0; PlayerControllerIndex < NumberOfPlayers; ++PlayerControllerIndex)
+	{
+		TAutoWeakObjectPtr<APlayerController> ActualPlayer = *PlayerControllers;
+		ACharacter* PlayerCharacter = ActualPlayer->GetCharacter();
+		PlayerCharacter->GetMesh()->MasterPoseComponent;
+		OriginalCharacterInstances.Add(PlayerCharacter);
+		PlayerControllers++;
+	}
+
+	for (AActor *Object : Objects)
+	{
+		OriginalObjectInstances.Add(Object);
+	}
+}
+
+void AComponentController::CreateCopiedInstances()
+{
+	RightTranslation = World->GetComponentsBoundingBox().GetSize().Y;
+	FrontTranslation = World->GetComponentsBoundingBox().GetSize().X;
+
+	//Right  
+
+	CreateCopiedInstancesWithTranslation(0, RightTranslation);
+
+	//Left  
+
+	CreateCopiedInstancesWithTranslation(0, -RightTranslation);
+
+	//Front  
+
+	CreateCopiedInstancesWithTranslation(FrontTranslation, 0);
+
+	//Behind  
+
+	CreateCopiedInstancesWithTranslation(-FrontTranslation, 0);
+
+	//Front-Right  
+
+	CreateCopiedInstancesWithTranslation(FrontTranslation, RightTranslation);
+
+	//Front-Left  
+
+	CreateCopiedInstancesWithTranslation(FrontTranslation, -RightTranslation);
+
+	//Behind-Right  
+
+	CreateCopiedInstancesWithTranslation(-FrontTranslation, RightTranslation);
+
+	//Behind-Left  
+
+	CreateCopiedInstancesWithTranslation(-FrontTranslation, -RightTranslation);
+
+
+}
+
+void AComponentController::CreateCopiedInstancesWithTranslation(float XTranslation, float YTranslation)
+{
+
+	FActorSpawnParameters WorldParameters;
+	WorldParameters.Template = World;
+	AStaticMeshActor * TranslatedWorld = GetWorld()->SpawnActor<class AStaticMeshActor>(World->GetClass(), WorldParameters);
+	TranslatedWorld->SetMobility(EComponentMobility::Movable);
+
+	FVector WorldLocationToTranslate = TranslatedWorld->GetActorLocation();
+	WorldLocationToTranslate.X += XTranslation;
+	WorldLocationToTranslate.Y += YTranslation;
+	TranslatedWorld->SetActorLocation(WorldLocationToTranslate);
+
+	ArrayOfTranslatedObject TranslatedCopiedObjectInstances;
+	ArrayOfTranslatedCharacter TranslatedCopiedCharacterInstances;
+	ArrayOfTranslatedObject TranslatedCopiedBulletInstances;
+
+	TranslatedCopiedObjectInstances.Value = TranslatedWorld->GetActorLocation() - World->GetActorLocation();
+	TranslatedCopiedCharacterInstances.Value = TranslatedWorld->GetActorLocation() - World->GetActorLocation();
+	TranslatedCopiedBulletInstances.Value = TranslatedWorld->GetActorLocation() - World->GetActorLocation();
+
+	for (AActor* Object : OriginalObjectInstances)
+	{
+		TranslatedCopiedObjectInstances.Key.Add(SpawnCopy(Object, TranslatedCopiedObjectInstances.Value, true));
+	}
+
+	for (AActor* Character : OriginalCharacterInstances)
+	{
+		AActor* ToAdd = Cast<AActor>(SpawnCopy(Character, TranslatedCopiedCharacterInstances.Value));
+		TranslatedCopiedCharacterInstances.Key.Add(Cast<ACharacter>(ToAdd));
+	}
+
+	CopiedObjectInstances.Add(TranslatedCopiedObjectInstances);
+	CopiedCharacterInstances.Add(TranslatedCopiedCharacterInstances);
+	CopiedBulletInstances.Add(TranslatedCopiedBulletInstances);
+}
+
+AActor* AComponentController::SpawnCopy(AActor* OriginalObject, FVector Translation, bool SetTemplate)
+{
+	if (OriginalObject == nullptr) return nullptr;
+
+	FActorSpawnParameters ObjectParameters;
+	AActor* TranslatedObject;
+	if (SetTemplate)
+	{
+		ObjectParameters.Template = OriginalObject;
+		TranslatedObject = GetWorld()->SpawnActor<class AActor>(OriginalObject->GetClass(), ObjectParameters);
+	}
+	else
+	{
+		TranslatedObject = GetWorld()->SpawnActor<class AActor>(OriginalObject->GetClass(), OriginalObject->GetActorTransform(), ObjectParameters);
+	}
+
+	FVector ObjectLocationToTranslate = TranslatedObject->GetActorLocation();
+	ObjectLocationToTranslate += Translation;
+	TranslatedObject->SetActorLocation(ObjectLocationToTranslate);
+
+	return TranslatedObject;
+}
+
+void AComponentController::TeleportObjectsIfEscaping()
+{
+	for (AActor* Object : OriginalObjectInstances)
+	{
+		TeleportIfEscaping(Object);
+	}
+	for (AActor* Object : OriginalCharacterInstances)
+	{
+		TeleportIfEscaping(Object);
+	}
+	for (AActor* Object : OriginalBulletInstances)
+	{
+		TeleportIfEscaping(Object);
+	}
+}
+
+void AComponentController::TeleportIfEscaping(AActor* Object)
+{
+
+	if (Object == nullptr) return;
+
+	if (PositiveX->IsOverlappingActor(Object))
+	{
+		Object->SetActorLocation(Object->GetActorLocation() + FVector(-FrontTranslation, 0, 0));
+	}
+	else if (NegativeX->IsOverlappingActor(Object))
+	{
+		Object->SetActorLocation(Object->GetActorLocation() + FVector(FrontTranslation, 0, 0));
+	}
+
+	if (PositiveY->IsOverlappingActor(Object))
+	{
+		Object->SetActorLocation(Object->GetActorLocation() + FVector(0, -RightTranslation, 0));
+	}
+	else if (NegativeY->IsOverlappingActor(Object))
+	{
+		Object->SetActorLocation(Object->GetActorLocation() + FVector(0, RightTranslation, 0));
+	}
+}
+
+void AComponentController::TranslateObject(AActor* ToTranslate, AActor* OriginalObject, const FVector &TranslateValue)
+{
+	if (ToTranslate != nullptr && ToTranslate->IsValidLowLevel())
+	{
+		if (OriginalObject->IsValidLowLevel())
+		{
+			FTransform TransformToTranslate = OriginalObject->GetActorTransform();
+			TransformToTranslate.SetLocation(TransformToTranslate.GetLocation() - TranslateValue);
+			ToTranslate->SetActorTransform(TransformToTranslate);
+		}
+	}
+}
+
+
---
+++ Source/ShootMeAround/ComponentController.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ComponentController.generated.h"
+
+struct FVector;
+template<typename InElementType, typename InAllocator> class TArray;
+template<typename InKeyType, typename InValueType>  class TPair;
+class AActor;
+class UBillboardComponent;
+class PlayerController;
+class AStaticMeshActor;
+
+typedef FVector TranslateFromOrigin;
+typedef TPair<TArray<AActor*>, TranslateFromOrigin> ArrayOfTranslatedObject;
+typedef TPair<TArray<ACharacter*>, TranslateFromOrigin> ArrayOfTranslatedCharacter;
+
+UCLASS(Blueprintable, BlueprintType)
+class SHOOTMEAROUND_API AComponentController : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	static const float MAX_TIMER_UPDATE_COPY;
+
+	UPROPERTY(Category = "Components", VisibleAnywhere, BlueprintReadOnly)
+		UBillboardComponent *Billboard;
+
+	UPROPERTY(Category = "World", EditAnywhere, BlueprintReadWrite)
+		AStaticMeshActor* World;
+
+	UPROPERTY(Category = "Players", EditAnywhere, BlueprintReadWrite)
+		int NumberOfPlayers;
+
+	UPROPERTY(Category = "Objects", EditAnywhere, BlueprintReadWrite)
+		TArray<AActor*> Objects;
+
+	UPROPERTY(Category = "Walls", EditAnywhere, BlueprintReadWrite)
+		AActor* PositiveX;
+	UPROPERTY(Category = "Walls", EditAnywhere, BlueprintReadWrite)
+		AActor* NegativeX;
+	UPROPERTY(Category = "Walls", EditAnywhere, BlueprintReadWrite)
+		AActor* PositiveY;
+	UPROPERTY(Category = "Walls", EditAnywhere, BlueprintReadWrite)
+		AActor* NegativeY;
+
+	UFUNCTION(BlueprintCallable, Category = "Bullets")
+		void AddBullet(AActor* NewBullet);
+
+private:
+
+	float RightTranslation;
+	float FrontTranslation;
+
+	TArray<AActor*> OriginalObjectInstances;
+	TArray<ArrayOfTranslatedObject> CopiedObjectInstances;
+
+	TArray<ACharacter*> OriginalCharacterInstances;
+	TArray<ArrayOfTranslatedCharacter> CopiedCharacterInstances;
+
+	TArray<AActor*> OriginalBulletInstances;
+	TArray<ArrayOfTranslatedObject> CopiedBulletInstances;
+
+
+public:
+
+	AComponentController();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	void UpdateCopyTransform();
+
+	void FillOriginalObjectsArray();
+
+	void CreateCopiedInstances();
+	void CreateCopiedInstancesWithTranslation(float XTranslation, float YTranslation);
+	AActor* AComponentController::SpawnCopy(AActor* OriginalObject, FVector Translation, bool SetTemplate = false);
+
+	void TeleportObjectsIfEscaping();
+	void TeleportIfEscaping(AActor *Object);
+
+	void TranslateObject(AActor* ToTranslate, AActor* OriginalObject, const FVector &TranslateValue);
+
+};
